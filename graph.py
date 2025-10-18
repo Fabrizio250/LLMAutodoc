7845d1337c9fb7b824c707c5f4ddd7d0e3299b44
Fabrizio250 <fabriziorosmarino@gmail.com>
2025-10-18 12:59:18 +0200

    update graph.py file

--- graph.py
+++ graph.py
@@ -1,8 +1,8 @@
 from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode
 from states import State
-from nodes import init_config, simple_llm_node,compile_artifacts_node
-from tools import clone_repo, ask_user_input, read_text_file, list_repo_files
+from nodes import init_config, llm_orchestrator,compile_artifacts_node,harvest_tool_results, generate_contents
+from tools import clone_repo, description, detectDatasetOwners,detectDatasetStatus
 from langchain_core.messages import AIMessage
 
 def _has_tool_calls(state: State) -> bool:
@@ -10,34 +10,41 @@ def _has_tool_calls(state: State) -> bool:
     if not msgs:
         return False
     last = msgs[-1]   # ultimo messaggio
-    return isinstance(last, AIMessage) and bool(getattr(last, "tool_calls", None)) # vai ai tool SOLO se l'ultimo è AIMessage *è* contiene almeno una tool_call
+    return isinstance(last, AIMessage) and bool(getattr(last, "tool_calls", None)) # vai a toolNode solo se l' ultimo AI message contiene una tool call
 
     
 
 
 def _router(state: State):
-    return "tools" if _has_tool_calls(state) else "compile"
+    return "tools" if _has_tool_calls(state) else "generate"
 
 
 def build_graph():
     graph = StateGraph(State)
     graph.add_node("setup", init_config)
-    graph.add_node("llm_orchestrator", simple_llm_node)
-    graph.add_node("toolsNode", ToolNode([clone_repo, ask_user_input, read_text_file, list_repo_files]))
+    graph.add_node("llm_orchestrator", llm_orchestrator)
+    graph.add_node("toolsNode", ToolNode([clone_repo, description, detectDatasetOwners,detectDatasetStatus]))
     graph.add_node("compilerNode", compile_artifacts_node)
+    graph.add_node("harvesterNode",harvest_tool_results)
+    graph.add_node("generatorNode",generate_contents)
 
 
     graph.set_entry_point("setup")
     graph.add_edge("setup", "llm_orchestrator")
+    graph.add_edge("toolsNode","harvesterNode")
+    graph.add_edge("harvesterNode", "llm_orchestrator")
     graph.add_edge("compilerNode", END)
+    graph.add_edge("generatorNode", "compilerNode")
+
 
 
     graph.add_conditional_edges("llm_orchestrator", _router, {
         "tools": "toolsNode",
-        "compile": "compilerNode"
+        "generate": "generatorNode"
     })
-    graph.add_edge("toolsNode", "llm_orchestrator")
 
     return graph.compile()
+
+
 
 
